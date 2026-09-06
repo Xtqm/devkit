d9f821a60e749e40799d7947de13c99066cabb44
TheQMechanic <133138386+TheQMechanic@users.noreply.github.com>
2026-09-06 13:11:41 -0400

    Initialize project structure with essential files and configurations

---
+++ main.py
@@ -0,0 +1,8 @@
+# Script to print every file inside of a folder
+import os # Import the os module to interact with file system
+# Ask user for input, folder path to process
+folder_name = input("Enter folder path: ")
+# List all files in the specified folder
+for root, dirs, files in os.walk(folder_name):
+    for file in files:
+        print(os.path.join(root, file))
