10e6d368a040d8a127c7effff40993b10e12811a
TheQMechanic <133138386+TheQMechanic@users.noreply.github.com>
2026-09-06 13:23:14 -0400

    Fixed typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os # Import the os module to interact with file system
 # Ask user for input, folder path to process
 folder_name = input("Enter folder path: ")
 # List all files in the specified folder
-print("File found:")
+print("Files found:")
 for root, dirs, files in os.walk(folder_name):
     for file in files:
         print(os.path.join(root, file))
